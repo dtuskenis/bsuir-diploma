d750ef80b8ba5127b455459a790e8f03b96a2665
Denis Tuskenis <d.tuskenis@sdventures.com>
2015-05-23 14:04:39 +0300

    fix table view cells margins on ios 8

--- src/ui/controls/TableViewCell/TableViewCell.m
+++ src/ui/controls/TableViewCell/TableViewCell.m
@@ -27,4 +27,23 @@
     self.contentView.frame = bounds;
 }
 
+- (void)willMoveToSuperview:(UIView *)newSuperview {
+    [super willMoveToSuperview:newSuperview];
+    
+    // Remove seperator inset
+    if ([self respondsToSelector:@selector(setSeparatorInset:)]) {
+        [self setSeparatorInset:UIEdgeInsetsZero];
+    }
+    
+    // Prevent the cell from inheriting the Table View's margin settings
+    if ([self respondsToSelector:@selector(setPreservesSuperviewLayoutMargins:)]) {
+        [self setPreservesSuperviewLayoutMargins:NO];
+    }
+    
+    // Explictly set your cell's layout margins
+    if ([self respondsToSelector:@selector(setLayoutMargins:)]) {
+        [self setLayoutMargins:UIEdgeInsetsZero];
+    }
+}
+
 @end
